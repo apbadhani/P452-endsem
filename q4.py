10a25f82f775212ab504fe6cba4df1d4740ce1b5
apoorv badhani <apoorv.badhani@niser.ac.in>
2022-05-20 13:02:45 +0530

    Done

--- q4.py
+++ q4.py
@@ -1,9 +1,10 @@
 from MNL import *
 import math
 #--------Defining potential function---------#
-def f(x):
+def pote(x):
     return 1.0/math.sqrt(1+x**2)
 #---------Legendre zeros and weights------#
+#------------ This function takes degree as the input and returns the set of zeros and weights-------# 
 def leg_zero_wei(deg):
     if deg == 4:
         return [0.861136311, 0.339981043, -0.339981043, -0.861136311],[0.347854845, 0.652145154, 0.652145154, 0.347854845]
@@ -22,9 +23,10 @@ def gaus_lege(f,n):
     return s
 num = [4,5,6]
 for i in num:
-    print("For degree ",i,", Potential is :",gaus_lege(f,i))
+    print("For degree ",i,", Potential is :",gaus_lege(pote,i))
 
-###For degree  4 , Potential is : 1.7620541789046658
+#-----------Results----------#
+#For degree  4 , Potential is : 1.7620541789046658
 #For degree  5 , Potential is : 1.7628552954010728
 #For degree  6 , Potential is : 1.762730048499759
 
